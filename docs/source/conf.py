ef782558039fffea945b24e8a6d067a4e4d6821b
bouncingbunyip <chris@ibunyip.com>
2022-10-19 13:27:24 -0700

    Update conf.py
    changed name and copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'BunyipFormBuilder'
+copyright = '2022, Chris Hubbard'
+author = 'Chris Hubbard'
 
 release = '0.1'
 version = '0.1.0'
